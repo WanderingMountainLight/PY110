b80684941ba5bcc672cc4ddeac864563b6312bb3
Steve Milanes <209536378+WanderingMountainLight@users.noreply.github.com>
2026-07-29 10:48:32 -0400

    Add lesson 2 + 3 subfolders

---
+++ lesson_3/debug_test.py
@@ -0,0 +1,13 @@
+# debug_test.py
+def double(x):
+    result = x * 2
+    return result
+
+def triple(x):
+    result = x * 3
+    return result
+
+total = 0
+total = double(total + 5)
+total = triple(total)
+print(total)
---
+++ lesson_3/tic_tac_toe/tictactoe.py
@@ -0,0 +1,37 @@
+
+
+def display_board(board):
+
+    print('')
+    print('       |        |        ')
+    print(f"    {board[1]}  |     {board[2]}  |  {board[3]}")
+    print('       |        |        ')
+    print('       |        |        ')
+    print('-------+--------+--------')
+    print('       |        |        ')
+    print(f"    {board[4]}  |    {board[5]} |  {board[6]}")
+    print('       |        |        ')
+    print('       |        |        ')
+    print('-------+--------+--------')
+    print('       |        |        ')
+    print(f"      {board[7]}  |      {board[8]}  |  {board[9]}")
+    print('       |        |        ')
+    print('       |        |        ')
+    print('')
+
+
+
+
+board = {
+  1: 'X', # top left
+  2: ' ', # top center
+  3: ' ', # top right
+  4: ' ', # middle left
+  5: 'O', # center
+  6: ' ', # middle right
+  7: ' ', # bottom left
+  8: ' ', # bottom center
+  9: 'X',  # bottom right
+}
+
+print(display_board(board))
